311a12a6346237feaff8ab28bcdb0ff23a1346aa
RyanNoscoe <99408961+RyanNoscoe@users.noreply.github.com>
2023-04-16 15:30:34 +0100

    Added html for a nav bar

---
+++ web-page/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <nav class="navbar">
+          <ol>
+            <li class="location"><a href="#"></a>Home</li>
+            <li class="location"><a href="#"></a>Explore</li>
+            <li class="location"><a href="#"></a>Notifications</li>
+            <li class="location"><a href="#"></a>Messages</li>
+            <li class="location"><a href="#"></a>Bookmarks</li>
+            <li class="location"><a href="#"></a>Lists</li>
+            <li class="location"><a href="#"></a>Profile</li>
+            <li class="location"><a href="#"></a>More</li>
+          </ol>
+        </nav>
+      </body>
+    </div>
+  );
+}
+
+export default App;
